8e1570aa23b9c6792a93202a42c62962d17e2c00
GabrielKono <gkonopnicki@gmail.com>
2023-04-17 09:15:16 +0100

    Update outofstock.py

--- outofstock.py
+++ outofstock.py
@@ -1,60 +1,312 @@
 import openpyxl
 import requests
+import os
+import time
+import random
+import logging
+import time
+import datetime
+import win32com.client as win32
+from tqdm import tqdm
 from bs4 import BeautifulSoup
+from openpyxl import Workbook
+from openpyxl import load_workbook
+from datetime import datetime
 
+def format_time(seconds):
+    hours, remainder = divmod(seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    return f"{int(hours)}h {int(minutes)}m {int(seconds)}s"
 
-def read_excel_file(file_path):
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+USER_AGENTS = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.0 Safari/605.1.15',
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Safari/605.1.15',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9',
+    'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.3',
+    'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.3',
+    'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:52.0) Gecko/20100101 Firefox/52.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; AS; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:63.0) Gecko/20100101 Firefox/63.0',
+    'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+    
+]
+
+def read_asins_from_excel(file_path):
     workbook = openpyxl.load_workbook(file_path)
     sheet = workbook.active
-    products = []
+    asins = []
 
     for row in sheet.iter_rows(min_row=2, values_only=True):
         asin = row[0]
-        url = f'https://www.amazon.co.uk/dp/{asin}'
-        products.append({'asin': asin, 'url': url})
+        asins.append(asin)
 
-    return products
+    return asins
 
 
-def check_availability(url):
+# Check the availability of a product by URL
+def check_availability(url, region):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        'User-Agent': random.choice(USER_AGENTS)
     }
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
+    for _ in range(3):  # Retry up to 3 times
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, 'lxml')
 
-    out_of_stock = soup.find('span', {'class': 'a-size-medium a-color-price'})
+            out_of_stock = soup.find('span', {'class': 'a-size-medium a-color-price'}) or \
+                           soup.find('div', {'id': 'availability'}) or \
+                           soup.find('span', {'class': 'a-size-medium a-color-state'})
 
-    if out_of_stock and 'out of stock' in out_of_stock.text.lower():
-        return 'yes'
-    else:
-        return 'no'
+            out_of_stock_phrases = get_out_of_stock_phrases(region)
+            if out_of_stock and any(phrase in out_of_stock.text.lower() for phrase in out_of_stock_phrases):
+                return 'Out Of Stock'
+            else:
+                return 'In Stock'
+
+        except requests.exceptions.HTTPError as e:
+            if e.response.status_code == 404:
+                logging.exception(f"URL not found: {url}")
+                return 'URL not found'
+            else:
+                logging.exception(f"HTTPError for URL: {url}")
+                time.sleep(5)
+    return 'Failed to fetch URL'
 
 
-def update_excel_file(file_path, products):
-    workbook = openpyxl.load_workbook(file_path)
+def generate_urls_for_asin(asin):
+    domains = {
+        'UK': 'https://www.amazon.co.uk/dp/',
+        'ES': 'https://www.amazon.es/dp/',
+        'FR': 'https://www.amazon.fr/dp/',
+        'DE': 'https://www.amazon.de/dp/',
+        'IT': 'https://www.amazon.it/dp/'
+    }
+    # Check for None values and print them
+    for region, domain in domains.items():
+        if domain is None:
+            print(f"Domain for region {region} is None")
+
+    urls = {region: domain + asin for region, domain in domains.items() if domain is not None}
+    return urls
+
+def get_out_of_stock_phrases(region):
+    phrases = {
+        'UK': ['out of stock', 'temporarily out of stock'],
+        'ES': ['agotado temporalmente', 'out of stock'],
+        'FR': ['temporairement en rupture de stock', 'out of stock'],
+        'DE': ['derzeit nicht auf lager', 'temporarily out of stock', 'out of stock'],
+        'IT': ['al momento non disponibile', 'out of stock'],
+    }
+    return phrases.get(region, [])
+
+# Determine the region based on the URL
+def get_region_from_url(url):
+    if 'amazon.es' in url:
+        return 'ES'
+    elif 'amazon.fr' in url:
+        return 'FR'
+    elif 'amazon.de' in url:
+        return 'DE'
+    elif 'amazon.it' in url:
+        return 'IT'
+    elif 'amazon.co.uk' in url:
+        return 'UK'
+
+# Write the results to a new output Excel file
+def write_results_to_excel(output_file_path, results):
+    workbook = openpyxl.Workbook()
     sheet = workbook.active
 
-    sheet.cell(row=1, column=2).value = 'URL'
-    sheet.cell(row=1, column=3).value = 'Out of Stock'
+    # Write the header row
+    sheet.cell(row=1, column=1).value = 'ASIN'
+    sheet.cell(row=1, column=2).value = 'Region'
+    sheet.cell(row=1, column=3).value = 'URL'
+    sheet.cell(row=1, column=4).value = 'Status'
 
-    for index, product in enumerate(products, start=2):
-        sheet.cell(row=index, column=2).value = product['url']
-        sheet.cell(row=index, column=3).value = product['out_of_stock']
+    # Write the results
+    for index, result in enumerate(results, start=2):
+        sheet.cell(row=index, column=1).value = result['asin']
+        sheet.cell(row=index, column=2).value = result['region']
+        sheet.cell(row=index, column=3).value = result['url']
+        sheet.cell(row=index, column=4).value = result['status']
 
-    workbook.save(file_path)
+    workbook.save(output_file_path)
 
+def load_or_create_workbook(file_path):
+    try:
+        workbook = load_workbook(file_path)
+    except FileNotFoundError:
+        workbook = Workbook()
+        workbook.save(file_path)
+        workbook = load_workbook(file_path)
+    return workbook
 
+def send_email(subject, body, attachment_path):
+    outlook = win32.Dispatch('outlook.application')
+    namespace = outlook.GetNamespace("MAPI")
+
+    mail = outlook.CreateItem(0)
+    mail.Subject = subject
+    mail.Body = body
+    mail.Attachments.Add(attachment_path)
+
+    # Add recipients (To and CC)
+    #mail.To = "mail@mail.com"  # Replace with the recipient's email address
+    #mail.CC = "mail@mail.com"  # Replace with the CC recipient's email address
+
+    # Use the following line to display the email before sending
+    mail.Display()
+
+    # Uncomment the following line to send the email
+    # mail.Send()
+
+    print("Email created in Outlook")
+
+def get_formatted_date():
+    today = datetime.now()
+    return today.strftime("ASINs Status %d%b%Y")
+
+def create_email_body(results, total_urls, total_asins):
+    total_results = len(results)
+    out_of_stock_count = sum(1 for result in results if result['status'] == 'Out Of Stock')
+    url_not_found_count = sum(1 for result in results if result['status'] == 'URL not found')
+
+    # Calculate percentages
+    out_of_stock_percentage = (out_of_stock_count / total_urls) * 100
+    url_not_found_percentage = (url_not_found_count / total_urls) * 100
+
+    body = f"""Hi,
+
+I hope this email finds you well. Please find the attached ASINs status report for today, {get_formatted_date()}.
+
+Summary of the report:
+- ASINS processed: {total_asins}
+- Total URLs processed: {total_urls}
+- Out of Stock: {out_of_stock_count} ({out_of_stock_percentage:.2f}%)
+- URL not found: {url_not_found_count} ({url_not_found_percentage:.2f}%)
+
+Please review the attached report for more detailed information. If you have any questions or need further assistance, don't hesitate to reach out.
+
+Best regards,
+
+[Your Name]
+[Your Position]
+"""
+
+    return body
+    
+# Main function
 def main():
-    file_path = 'C:/Users/gabriel.konopnicki/OneDrive - funko.com/input/list.xlsx'
-    products = read_excel_file(file_path)
+    total_asins_processed = 0
+    input_file_path = r'PATH FOR THE INPUT FILE'
 
-    for product in products:
-        product['out_of_stock'] = check_availability(product['url'])
+    # Generate the output file name based on today's date
+    today = datetime.today().strftime('%d%b%Y')
+    output_file_name = f"ASINS_Status_{today}.xlsx"
+    output_file_path = fr'PATH FOR THE OUTPUT FILE/{output_file_name}'
 
-    update_excel_file(file_path, products)
+    asins = read_asins_from_excel(input_file_path)
 
+    results = []
+    total_urls = len(asins) * 5  # Assuming 5 domains for each ASIN
+    total_asins = len(asins) 
+    urls_processed = 0
+    start_time = time.time()
 
+    # Initialize the progress bar
+    progress_bar = tqdm(total=total_urls, desc="Processing", unit="URL", ncols=125)
+
+    # Check the availability for each ASIN and each Amazon domain
+    for asin in asins:
+        urls = generate_urls_for_asin(asin)
+        for region, url in urls.items():
+            status = check_availability(url, region)
+            if status == "Out Of Stock" or status == "URL not found":
+                results.append({'asin': asin, 'region': region, 'url': url, 'status': status})
+
+            # Calculate progress information
+            urls_processed += 1
+            progress = (urls_processed / total_urls) * 100
+            elapsed_time = time.time() - start_time
+            time_per_url = elapsed_time / urls_processed
+            time_remaining = (total_urls - urls_processed) * time_per_url
+
+            # Update the progress bar
+            progress_bar.set_postfix({"ASIN": asin, "Region": region, "Status": status, "Time remaining": format_time(time_remaining)}, refresh=False)
+            progress_bar.update(1)
+
+            # Log progress information
+            logging.info(f"ASIN: {asin}, Region: {region}, URL: {url}, Status: {status}, Progress: {progress:.2f}%, Time remaining: {format_time(time_remaining)}")
+
+            time.sleep(10)  # Add a delay of 5 seconds between requests
+        total_asins_processed += 1
+
+    # Load or create the output workbook and write the results
+    load_or_create_workbook(output_file_path)
+    write_results_to_excel(output_file_path, results)
+
+    # Create and send an email with the output file attached
+    subject = get_formatted_date()
+    body = create_email_body(results, total_urls, total_asins)
+    send_email(subject, body, output_file_path)
+
+    # Print the success message
+    print("Hooray! The process has been successfully completed.")
+
+# Run the main function
 if __name__ == '__main__':
     main()
